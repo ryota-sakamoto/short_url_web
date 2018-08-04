5658457f230858f76b906f9e83881fad8c08270c
ryota-sakamoto <sakamo.ryota+github@gmail.com>
2018-08-04 23:06:35 +0900

    add test

--- src/controller/util.rs
+++ src/controller/util.rs
@@ -23,3 +23,28 @@ pub fn sha256(s: &str) -> String {
     hasher.input_str(s);
     hasher.result_str()
 }
+
+#[test]
+fn generate_id_test() {
+    const ID_LEN: usize = 8;
+
+    let p1 = generate_id(ID_LEN);
+    let p2 = generate_id(ID_LEN);
+
+    assert!(p1 != p2);
+}
+
+#[test]
+fn generate_password_hash_test() {
+    let ph = generate_password_hash(None, "hash");
+    assert_eq!(ph, None);
+
+    let ph = generate_password_hash(Some("".to_string()), "\n");
+    assert_eq!(ph, Some("01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b".to_string()));
+}
+
+#[test]
+fn sha256_test() {
+    assert_eq!("01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b", sha256("\n"));
+    assert_eq!("6b3a55e0261b0304143f805a24924d0c1c44524821305f31d9277843b8a10f4e", sha256("password\n"));
+}
